f116d6d5dda8214fb2b68bcbbe29fe989c8e774b
Jos <jozef.casteels@thomasmore.be>
2021-03-12 11:30:55 +0100

    OPLOSSING

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,16 @@
 public class Test {
     public static void main(String[] args) {
         Wachtrij wachtrij = new Wachtrij();
-        System.out.println(wachtrij);  // wachtrij = wachtrij.Tostring()
+        wachtrij.voegToe("Marc");
+        wachtrij.voegToe("Jos");
+        System.out.println(wachtrij);  // rij = rij.Tostring()
+        wachtrij.bediend("Joris");
+        wachtrij.bediend("Lars");
+        System.out.println(wachtrij);
+        wachtrij.overzichtBedienden();
+        wachtrij.overzichtWachtenden();
+        wachtrij.tellen();
     }
 }
+
 
--- src/Wachtrij.java
+++ src/Wachtrij.java
@@ -8,33 +8,67 @@ public class Wachtrij {
         rij = new HashMap<>();
         rij.put("Lars", "WACHTEND");
         rij.put("Anne", "WACHTEND");
-        //...
     }
 
     // Persoon toevoegen aan de wachtrij
     public void voegToe(String naam) {
-
+        rij.put(naam, "WACHTEND");
     }
 
 
     // Status van WACHTEND naar BEDIEND veranderen
     public void bediend(String naam) {
+        if (rij.containsKey(naam)) {
+            rij.put(naam, "BEDIEND");
+        } else {
+            System.out.println("naam bestaat niet");
+        }
 
+        //       Alternatief
+        //       if (rij.replace(naam, "BEDIEND") == null) {
+        //       System.out.println("naam bestaat niet");
+        //       }
+        // testen of de naam wel degelijk voorkomt?
+        // rij.replace(naam, "BEDIEND");
+        // als uw key er reeds in staat, dan wordt de waarde overschreven
     }
 
     //Maak een overzicht van de bediende personen
     public void overzichtWachtenden() {
-
+        System.out.println("De wachtende mensen");
+        for (String naam : rij.keySet()) {
+            if (rij.get(naam).equals("WACHTEND")) //rij.get(naam) = geeft de value = BEDIEND of WACHTEND
+            {
+                System.out.println(naam);
+            }
+        }
     }
 
     // Maak een overzicht van de wachtende personen
     public void overzichtBedienden() {
+        System.out.println("De bediende mensen");
+        for (String naam : rij.keySet()) {
+            if (rij.get(naam).equals("BEDIEND")) //rij.get(naam) = geeft de value = BEDIEND of WACHTEND
+            {
+                System.out.println(naam);
+            }
+        }
     }
 
     // Tellen hoeveel wachtende en hoeveel bediende personen
     public void tellen() {
         int tellerW = 0;
         int tellerB = 0;
+        for (String naam : rij.keySet()) {
+            if (rij.get(naam).equals("WACHTEND")) //rij.get(naam) = geeft de value = BEDIEND of WACHTEND
+            {
+                tellerW++;
+            } else {
+                tellerB++;
+            }
+        }
+        System.out.println("aantal wachtenden " + tellerW);
+        System.out.println("aantal bedienden " + tellerB);
     }
 
     public String toString() {
